73304d9f5a60126cbfac23531db310032ee9011e
Gad Nadjar <gadnadjar@gmail.com>
2025-01-07 16:27:37 +0200

    fix bug fetch endpoint

--- index.js
+++ index.js
@@ -90,10 +90,12 @@ app.get('/profile', (req, res) => {
 // ---------------------upload photos---------------------
 app.post('/upload-by-link', async (req, res) => {
     const { link } = req.body;
+    const newName = 'photo' + Date.now() + '.jpg'
     await imageDownloader.image({
         url: link,
-        dest: __dirname + '/uploads'
+        dest: __dirname + '/uploads/' + newName,
     })
+    res.json(newName);
 })
 // -------------------------------------------------------
 
